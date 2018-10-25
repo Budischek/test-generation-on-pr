9ad9434eadacc0a9160d1c2ce5836fe24e6c283c
David Budischek <david.budischek@gmail.com>
2018-10-25 12:20:13 +0900

    Initial Commit

---
+++ src/test/java/kr/ac/kaist/testonpr/TestOnPrApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.ac.kaist.testonpr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestOnPrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
